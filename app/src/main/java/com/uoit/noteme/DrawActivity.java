1694277bc2b0f24d0fe11d256238467c5876f556
Alexandre <alexandre@dafoe.me>
2021-11-23 16:08:05 -0500

    more drawing features

--- app/src/main/java/com/uoit/noteme/DrawActivity.java
+++ app/src/main/java/com/uoit/noteme/DrawActivity.java
@@ -1,18 +1,28 @@
 package com.uoit.noteme;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Path;
+import android.graphics.*;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrawActivity extends AppCompatActivity {
 
     drawingCanvas canvas;
+    String[] itemTypes = {"Circle", "Square", "Line"};
+    String[] itemSize = {"1", "3", "5", "10", "20", "30", "40", "50", "75", "100"};
+    Spinner itemTypeSpinner;
+    Spinner itemSizeSpinner;
+
+    Float lineStartx = null;
+    Float lineStarty = null;;
+    ArrayList<float[]> lines = new ArrayList<float[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +33,29 @@ public class DrawActivity extends AppCompatActivity {
         canvas = new drawingCanvas(this);
         parent.addView(canvas);
 
-        //drawingCanvas.setContent
+        //Populate Spinners
+        itemTypeSpinner = (Spinner) findViewById(R.id.itemTypeView);
+        itemSizeSpinner = (Spinner) findViewById(R.id.itemSizeView);
+
+        ArrayAdapter<String> adapterTypes = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemTypes);
+        adapterTypes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemTypeSpinner.setAdapter(adapterTypes);
+
+        ArrayAdapter<String> adapterSize = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemSize);
+        adapterSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemSizeSpinner.setAdapter(adapterSize);
 
     }
 
     private class drawingCanvas extends View {
         Paint circlePaint;
+        Paint linePaint;
         Path circlePath;
         public drawingCanvas(Context context){
             super(context);
 
             circlePaint = new Paint();
+            linePaint = new Paint();
             circlePath = new Path();
         }
 
@@ -41,12 +63,52 @@ public class DrawActivity extends AppCompatActivity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            canvas.drawPath( circlePath,  circlePaint);
+            for (int i = 0; i < lines.size(); i++) {
+                Log.d("Debug", "Drawing line " + String.valueOf(i));
+                linePaint.setStrokeWidth(lines.get(i)[4]);
+                canvas.drawLine(lines.get(i)[0], lines.get(i)[1], lines.get(i)[2], lines.get(i)[3], linePaint);
+            }
+            canvas.drawPath(circlePath, circlePaint);
         }
 
         private void touchMove(float posx, float poxy){
-            //circlePath.reset();
-            circlePath.addCircle(posx, poxy, 20, Path.Direction.CW);
+            float size = Float.parseFloat(itemSizeSpinner.getSelectedItem().toString());
+
+            String type = itemTypeSpinner.getSelectedItem().toString();
+            switch (type){
+                case "Circle":
+                    circlePath.addCircle(posx, poxy, size, Path.Direction.CW);
+                    break;
+                case "Square":
+                    RectF rect = new RectF(posx - size, poxy + size, posx + size, poxy - size);
+                    circlePath.addRect(rect, Path.Direction.CW);
+                    break;
+            }
+        }
+
+        private void drawLine(float posx, float posy){
+            float size = Float.parseFloat(itemSizeSpinner.getSelectedItem().toString());
+            String type = itemTypeSpinner.getSelectedItem().toString();
+
+            if (type.equals("Line")){
+                //Line not started
+                if (lineStartx == null && lineStarty == null){
+                    Log.d("DEBUG", "Line started");
+                    lineStartx = posx;
+                    lineStarty = posy;
+                } else {
+                    Log.d("DEBUG", "Line ended");
+                    lines.add(new float[]{lineStartx, lineStarty, posx, posy, size});
+                    canvas.invalidate();
+                    drawLineReset();
+                }
+            }
+
+        }
+
+        private void drawLineReset(){
+            lineStartx = null;
+            lineStarty = null;
         }
 
         @Override
@@ -56,17 +118,13 @@ public class DrawActivity extends AppCompatActivity {
 
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    //touch_start(x, y);
-                    canvas.invalidate();
-                    //postInvalidate();
+                    drawLine(x, y);
                     break;
                 case MotionEvent.ACTION_MOVE:
                     canvas.touchMove(x, y);
                     canvas.invalidate();
                     break;
                 case MotionEvent.ACTION_UP:
-                    //touch_up();
-                    //invalidate();
                     break;
             }
             return true;
